4b940346bad99feadfb0dc538d401360d1a3461b
Fatemeh <f.khalleghi.777@gmail.com>
2024-10-08 20:04:54 +0200

    variable 3

---
+++ src/main/java/se/lexicon/OperatorsDemo.java
@@ -0,0 +1,11 @@
+package se.lexicon;
+
+public class OperatorsDemo {
+    public static void main(String[] args) {
+        int numberOne = 2;
+        int numberTwo = 5;
+        int sumResult = numberOne + numberTwo;
+        System.out.println(sumResult);
+    }
+}
+
