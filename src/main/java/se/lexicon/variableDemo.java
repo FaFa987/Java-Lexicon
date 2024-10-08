6463102afc1726e92978d9a0347b7b8336734c46
Fatemeh <f.khalleghi.777@gmail.com>
2024-10-08 14:53:11 +0200

    variable 1

---
+++ src/main/java/se/lexicon/variableDemo.java
@@ -0,0 +1,17 @@
+package se.lexicon;
+
+public class variableDemo {
+    public static void main(String[] args) {
+        long balance = 500000;
+        balance = 500000;
+        double area = 25.58;
+        float radius = 5.5f;
+        char grader = 'A';
+        char gradeUn = '\u2305';
+        int accountNumber = 10000;
+        boolean isLoggedIn;
+        System.out.println("Balance: " + balance);
+        System.out.println("Area: " + area);
+
+    }
+}
